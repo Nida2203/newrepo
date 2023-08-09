fa4ce1d2abe24b8b1e2b5ac55a33d274f4cfd347
Nida2203 <nidahudli15@gmail.com>
2023-08-09 18:43:31 +0530

    practise

--- src/main/java/com/example/newrepo/practise.java
+++ src/main/java/com/example/newrepo/practise.java
@@ -1,4 +1,4 @@
-package com.example.makemytripms;
+package com.example.newrepo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
